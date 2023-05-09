640395051afd3a4f7b93954c5cff038cc2be2fdd
Lukeh Mishr <lukeshmishr2@outlook.com>
2023-05-09 11:33:27 -0400

    updated the serp

---
+++ app/Data/SearchResult.cs
@@ -0,0 +1,12 @@
+namespace app.Data;
+
+public class SearchResult
+{
+    public String? Title { get; set; }
+
+    public String? Price { get; set; }
+
+    public String? Description {get; set; }
+
+    public String? Thumbnail  { get; set; }
+}
